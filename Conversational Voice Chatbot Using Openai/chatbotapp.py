986243f58ef0ea8a1b4e713e946b992f91e831bb
Muhammad Rafay <58334014+RAFAY123415@users.noreply.github.com>
2024-01-08 02:57:25 +0500

    Coversational Openai Voice Chatbot

---
+++ Conversational Voice Chatbot Using Openai/chatbotapp.py
@@ -0,0 +1,53 @@
+import streamlit as st
+from utils import get_answer, text_to_speech, autoplay_audio, speech_to_text
+from audio_recorder_streamlit import audio_recorder
+from streamlit_float import *
+import os
+
+# Initialize floating features for the interface
+float_init()
+
+# Initialize session state for managing chat messages
+def initialize_session_state():
+    if "messages" not in st.session_state:
+        st.session_state.messages = [{"role": "assistant", "content": "Hi! How may I assist you today?"}]
+
+initialize_session_state()
+
+st.title("OpenAI Conversational Voice Chatbot")
+
+# Create a container for the microphone and audio recording
+footer_container = st.container()
+with footer_container:
+    audio_bytes = audio_recorder()
+
+
+if audio_bytes:
+    with st.spinner("Transcribing..."):
+        # Write the audio bytes to a temporary file
+        webm_file_path = "temp_audio.mp3"
+        with open(webm_file_path, "wb") as f:
+            f.write(audio_bytes)
+
+        # Convert the audio to text using the speech_to_text function
+        transcript = speech_to_text(webm_file_path)
+        if transcript:
+            st.session_state.messages.append({"role": "user", "content": transcript})
+            with st.chat_message("user"):
+                st.write(transcript)
+            os.remove(webm_file_path)
+
+
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking🤔..."):
+            final_response = get_answer(st.session_state.messages)
+        with st.spinner("Generating audio response..."):    
+            audio_file = text_to_speech(final_response)
+            autoplay_audio(audio_file)
+        st.write(final_response)
+        st.session_state.messages.append({"role": "assistant", "content": final_response})
+        os.remove(audio_file)
+
+# Float the footer container and provide CSS to target it with
+footer_container.float("bottom: 0rem;")
